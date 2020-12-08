24eec59eea8444b50b5d65e2b344ea86430b1de9
Malina228 <71219885+Malina228@users.noreply.github.com>
2020-12-08 21:38:06 +0300

    добавляю ещё один поток, который считает сумму элементов массива, синхронизирую одним мьютексом

--- SPO/L5/L5.cpp
+++ SPO/L5/L5.cpp
@@ -1,4 +1,4 @@
-﻿//  В2
+// В2
 
 #include <iostream>
 #include <windows.h>
@@ -6,11 +6,26 @@
 using namespace std;
 
 
+DWORD WINAPI thread3_F(LPVOID mass)
+{
+	HANDLE thread_mutex = OpenMutex(SYNCHRONIZE, FALSE, L"Mutex");
+	WaitForSingleObject(thread_mutex, INFINITE);
+	int summa = 0;
+	cout << "загрузился поток 3" << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		summa += *((int*)mass + i);
+	}
+	cout << "сумма: " << summa << endl;
+	cout << endl << endl;
+	ReleaseMutex(thread_mutex);
+	return 1;
+}
 
 DWORD WINAPI thread2_F(LPVOID mass)
 {
 	HANDLE thread_mutex = OpenMutex(SYNCHRONIZE, FALSE, L"Mutex");
-	cout << "загрузился поток 2" << endl;
 	WaitForSingleObject(thread_mutex, INFINITE);
 	if (*(int*)mass > *((int*)mass + 9))
 	{
@@ -19,7 +34,7 @@ DWORD WINAPI thread2_F(LPVOID mass)
 		*((int*)mass + 9) = *(int*)mass;
 		*(int*)mass = temp;
 	}
-
+	cout << "загрузился поток 2" << endl;
 	cout << "измененный массив: ";
 
 	for (int i = 0; i < 10; i++)
@@ -36,14 +51,13 @@ DWORD WINAPI thread1_F(LPVOID mass)
 {
 	HANDLE thread_mutex = OpenMutex(SYNCHRONIZE, FALSE, L"Mutex");
 	WaitForSingleObject(thread_mutex, INFINITE);
-	cout << "загрузился поток 1" << endl;
 	srand(time(0));
 	Sleep(1000);
 	for (int i = 0; i < 10; i++)
 	{
 		*((int*)mass + i) = rand() % 100;
 	}
-
+	cout << "загрузился поток 1" << endl;
 	cout << "исходный массив: ";
 
 	for (int i = 0; i < 10; i++)
@@ -65,10 +79,11 @@ int main()
 	Mutex = CreateMutex(NULL, FALSE, L"Mutex");
 	HANDLE thread1 = CreateThread(NULL, 0, thread1_F, (void*)mass, NULL, 0);
 	HANDLE thread2 = CreateThread(NULL, 0, thread2_F, (void*)mass, NULL, 0);
-	HANDLE Threads[2] = { thread1, thread2 };
-	WaitForMultipleObjects(2, Threads, TRUE, INFINITE);
+	HANDLE thread3 = CreateThread(NULL, 0, thread3_F, (void*)mass, NULL, 0);
+	HANDLE Threads[3] = { thread1, thread2, thread3 };
+	WaitForMultipleObjects(3, Threads, TRUE, INFINITE);
 	CloseHandle(Mutex);
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 3; i++)
 	{
 		CloseHandle(Threads[i]);
 	}
