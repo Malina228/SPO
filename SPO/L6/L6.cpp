db8a261af133332fed55e7921ffe3915cabb5c87
Malina228 <71219885+Malina228@users.noreply.github.com>
2020-12-08 21:39:52 +0300

    добавляю 2 потока, один ищет минимальное значение массива, второй - максимальное, синхронизирую одним семафором

--- SPO/L6/L6.cpp
+++ SPO/L6/L6.cpp
@@ -1,4 +1,4 @@
-﻿//  В2
+// В2
 
 #include <iostream>
 #include <windows.h>
@@ -18,7 +18,8 @@ DWORD WINAPI thread1_F(LPVOID mass)
 		*((int*)mass + i) = rand() % 500;
 		Sleep(1000);
 		cout << *((int*)mass + i) << "  ";
-		ReleaseSemaphore(semafor, 1, NULL);
+		ReleaseSemaphore(semafor, 3, NULL);
+		Sleep(200);
 	}
 	return 1;
 }
@@ -38,17 +39,51 @@ DWORD WINAPI thread2_F(LPVOID mass)
 	return 1;
 }
 
+DWORD WINAPI thread3_F(LPVOID mass)
+{
+	int max = *((int*)mass + 0);
+	for (int i = 0; i < N; i++)
+	{
+		WaitForSingleObject(semafor, INFINITE);
+		if (*((int*)mass + i) > max)
+			max = *((int*)mass + i);
+		Sleep(500);
+	}
+
+	cout << "max: " << max << endl;
+
+	return 1;
+}
+
+DWORD WINAPI thread4_F(LPVOID mass)
+{
+	int min = *((int*)mass + 0);
+	for (int i = 0; i < N; i++)
+	{
+		WaitForSingleObject(semafor, INFINITE);
+		if (*((int*)mass + i) < min)
+			min = *((int*)mass + i);
+		Sleep(500);
+	}
+
+	cout << "min: " << min << endl;
+
+	return 1;
+}
+
 
 int main()
 {
 	int mass[N];
 	setlocale(LC_ALL, "rus");
-	semafor = CreateSemaphore(NULL, 0, N, L"semafor");
+	semafor = CreateSemaphore(NULL, 0, 3, L"semafor");
 	HANDLE thread1 = CreateThread(NULL, 0, thread1_F, (void*)mass, NULL, 0);
 	HANDLE thread2 = CreateThread(NULL, 0, thread2_F, (void*)mass, NULL, 0);
-	HANDLE Threads[2] = { thread1, thread2 };
-	WaitForMultipleObjects(2, Threads, TRUE, INFINITE);
-	for (int i = 0; i < 2; i++)
+	HANDLE thread3 = CreateThread(NULL, 0, thread3_F, (void*)mass, NULL, 0);
+	HANDLE thread4 = CreateThread(NULL, 0, thread4_F, (void*)mass, NULL, 0);
+	HANDLE Threads[4] = { thread1, thread2, thread3, thread4 };
+	WaitForMultipleObjects(4, Threads, TRUE, INFINITE);
+	for (int i = 0; i < 4; i++)
 	{
 		CloseHandle(Threads[i]);
 	}
